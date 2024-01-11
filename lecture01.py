ea673393b43c3675f474c2ecbcd7232ba106c305
massoudhamad <massoudhamad@gmail.com>
2024-01-11 15:47:17 +0300

    11012024

---
+++ lecture01.py
@@ -0,0 +1,8 @@
+def sumtwo(a,b):
+    return a+b
+
+print(sumtwo(2,3)*sumtwo(5,4))
+
+def sum(a,b):
+    print(a+b)
+sum(2,3)*sum(5,4)
---
+++ loop_lecture01.py
@@ -0,0 +1,12 @@
+"""for i in range(1,1001,1):
+    print(i)"""
+"""mysales = [3,5,1,4,1,5,1,3,4,2]
+for i in mysales:
+    print(i)"""
+
+sum=0
+for i in range(1,10):
+    if i%2 == 0:
+        print(i)
+        sum = sum+i
+print("Total Sum is ",sum)
