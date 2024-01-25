c4150407e215ef3a79d96cf5b3024fb40e8f4c69
massoudhamad <massoudhamad@gmail.com>
2024-01-25 12:22:53 +0300

    function and loops

---
+++ function.py
@@ -0,0 +1,25 @@
+""" def miles(km):
+    return km*0.6214
+
+def main():
+    km = int(input("Enter your km: "))
+    print(miles(km))
+
+main() """
+
+def estimator(feet,price):
+    number_of_gallons=feet/115
+    hours_required=(feet*8)/115
+    cost=number_of_gallons*price
+    labor_charges=hours_required*20
+    print("number_of_gallons=",number_of_gallons)
+    print("hours_required=",(feet*8)/115)
+    print("cost=",number_of_gallons*price)
+    print("labor_charges=",hours_required*20)
+    print("total_cost= ",cost+labor_charges)
+    
+def main():
+    feet=int(input("Enter your feet: "))
+    price=float(input("Enter your price: "))
+    estimator(feet,price)
+main()
--- loop_lecture01.py
+++ loop_lecture01.py
@@ -4,9 +4,99 @@
 for i in mysales:
     print(i)"""
 
-sum=0
+""" sum=0
 for i in range(1,10):
     if i%2 == 0:
         print(i)
         sum = sum+i
-print("Total Sum is ",sum)
+print("Total Sum is ",sum) """
+
+""" my_data=[3,6,7,19]
+for i in my_data:
+    print(i) """
+    
+#while loop
+""" 1-100 """
+"""i=1
+while(i>=1):
+    print(i)
+    i=i+2
+    """
+""" def convert(celcius):
+    return 1.8*celcius+32
+
+def main():
+    print("Celcius\t\t\tFahrenheit")
+    for i in range(0,21):
+        print(i,"\t\t\t",convert(i))
+main() """
+
+"""def summation(number):
+    sum=0
+    for i in range(1,number+1):
+        sum+=i #sum=sum+i
+    print("Summation of the number is: ",sum)
+
+def main():
+    number=int(input("Enter your number: "))
+    summation(number)
+main()"""
+
+#sentinel value
+"""def grade(score):
+    if(score<0 or score >100):
+        return "Invalid Marks"
+    else:
+        if(score>70):
+            return "A"
+        elif(score>60):
+            return "B+"
+        elif(score>50):
+            return "B"
+        elif(score>40):
+            return "C"
+        else:
+            return "F"
+    
+def calculateGrade(marks):
+    while marks != -1 :
+        print(grade(marks))
+        marks=int(input("Enter your marks: "))
+
+def main():
+    score=int(input("Enter your marks: "))
+    calculateGrade(score)
+
+main()"""
+
+#break and continue
+"""for i in range(1,10):
+    if i==5:
+        break
+    print(i)
+"""
+"""for i in range(1,10):
+    if i==5:
+        continue
+    print(i)"""
+
+"""i=1
+while(i<=10):
+    if i==5:
+        continue
+    print(i)
+    i=i+1"""
+    
+#nested loop
+for h in range(0,24):
+    for m in range(0,60):
+        for s in range(0,60):
+            print(h,":",m,":",s)    
+        
+
+
+    
+        
+
+
+    
